648a4a8bbe01c3b750b71e24f9ef7112a7323bd2
Leocaliban <leocaliban@gmail.com>
2019-03-18 06:23:37 -0300

    Cria toast para avisos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,21 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, ErrorHandler } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
-
 import { MessageService } from 'primeng/components/common/messageservice';
-import { ToastModule } from 'primeng/toast';
-import { TableModule } from 'primeng/table';
-import { TooltipModule } from 'primeng/tooltip';
-import { DialogModule } from 'primeng/dialog';
 
 import { OdontogramaComponent } from './odontograma/odontograma.component';
 import { OdontoComponent } from './odonto/odonto.component';
 import { AppComponent } from './app.component';
 
-import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 
 import { ROUTES } from './app.routes';
 import { CategoriaDespesaModule } from './categoria-despesa/categoria-despesa.module';
+import { ErrorHandlerService } from './core/services/error-handler.service';
 
 @NgModule({
   declarations: [
@@ -36,7 +29,6 @@ import { CategoriaDespesaModule } from './categoria-despesa/categoria-despesa.mo
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-
     HttpClientModule,
     CoreModule,
     CategoriaDespesaModule,
@@ -45,6 +37,11 @@ import { CategoriaDespesaModule } from './categoria-despesa/categoria-despesa.mo
   ],
   providers: [
     MessageService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorHandlerService,
+      multi: true
+    }
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
   bootstrap: [AppComponent]
