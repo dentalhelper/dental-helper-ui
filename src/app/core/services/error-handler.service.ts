b1ab9a2b3d9c5cbee675e8e955390d952c107174
Leocaliban <leocaliban@gmail.com>
2019-03-18 06:21:48 -0300

    Cria ErrorHandlerService e implementa o handle409

---
+++ src/app/core/services/error-handler.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { ToastService } from './toast.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorHandlerService implements HttpInterceptor {
+
+  constructor(
+    private toastService: ToastService
+  ) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(catchError((error, caught) => {
+      let errorObj = error;
+      if (errorObj.error) {
+        errorObj = errorObj.error[0];
+      }
+
+      switch (errorObj.status) {
+        case 409:
+          this.handle409(errorObj);
+          break;
+        default:
+
+          this.handleDefaultError(errorObj);
+      }
+
+      return throwError(error);
+    })) as any;
+  }
+
+  handle409(errorObj: any) {
+    this.toastService.exibirAviso(`${errorObj.mensagemUsuario}`);
+  }
+
+  handleDefaultError(errorObj: any) {
+    this.toastService.exibirErro('Ocorreu um erro inesperado.');
+  }
+
+}
