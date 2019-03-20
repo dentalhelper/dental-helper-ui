7c451ef753c3584d39c92f08b0998e7724c79b10
Leocaliban <leocaliban@gmail.com>
2019-03-20 05:38:51 -0300

    Cria DespesasPesquisaComponent

---
+++ src/app/despesas/despesas-pesquisa/despesas-pesquisa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-despesas-pesquisa',
+  templateUrl: './despesas-pesquisa.component.html',
+  styleUrls: ['./despesas-pesquisa.component.scss']
+})
+export class DespesasPesquisaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
