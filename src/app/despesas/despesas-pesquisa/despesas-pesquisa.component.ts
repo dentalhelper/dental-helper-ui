1e4565a6bf387e06d135f5d60cba9971e5a723a9
Leocaliban <leocaliban@gmail.com>
2019-03-20 21:46:06 -0300

    Implementa form na pesquisa de despesas

--- src/app/despesas/despesas-pesquisa/despesas-pesquisa.component.ts
+++ src/app/despesas/despesas-pesquisa/despesas-pesquisa.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+declare var $: any;
 @Component({
   selector: 'app-despesas-pesquisa',
   templateUrl: './despesas-pesquisa.component.html',
-  styleUrls: ['./despesas-pesquisa.component.scss']
+  styleUrls: ['./despesas-pesquisa.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DespesasPesquisaComponent implements OnInit {
+
+  categorias = [
+    { label: 'Alimentação', value: '1' },
+    { label: 'Transporte', value: '2' }
+  ];
+
+  status = [
+    { label: 'Todos', value: '1' },
+    { label: 'Pagos', value: '2' },
+    { label: 'Abertos', value: '3' }
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  criarDespesa(){
+  criarDespesa() {
     // TODO: Implementar a criação de despesa
   }
-
+  isMobile(): boolean {
+    return $.browser.mobile;
+  }
 }
--- src/app/despesas/despesas.module.ts
+++ src/app/despesas/despesas.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DespesasPesquisaComponent } from './despesas-pesquisa/despesas-pesquisa.component';
 import { RouterModule } from '@angular/router';
+
+import { DespesasPesquisaComponent } from './despesas-pesquisa/despesas-pesquisa.component';
 import { DESPESA_ROUTES } from './despesas.routes';
+
+import {FieldsetModule} from 'primeng/fieldset';
+import {DropdownModule} from 'primeng/dropdown';
+import {CalendarModule} from 'primeng/calendar';
+import {SelectButtonModule} from 'primeng/selectbutton';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    FieldsetModule,
+    DropdownModule,
+    CalendarModule,
+    SelectButtonModule,
     RouterModule.forChild(DESPESA_ROUTES)
   ],
   declarations: [DespesasPesquisaComponent]
