9ced6eabf245b7941344713b4362433223601060
Leocaliban <leocaliban@gmail.com>
2019-03-22 08:42:26 -0300

    Adiciona despesa ao menu e cria sua rota

--- src/app/despesas/despesas.routes.ts
+++ src/app/despesas/despesas.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { DespesasPesquisaComponent } from './despesas-pesquisa/despesas-pesquisa.component';
+import { DespesaCadastroComponent } from './despesa-cadastro/despesa-cadastro.component';
 
 export const DESPESA_ROUTES: Routes = [
   {
     path: '',
     component: DespesasPesquisaComponent
+  },
+  {
+    path: 'novo',
+    component: DespesaCadastroComponent
   }
 ];
