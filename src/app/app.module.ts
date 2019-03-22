88bdfef3a3a78999aaaea0c6d17f9f277c68ff9a
Leocaliban <leocaliban@gmail.com>
2019-03-21 21:36:39 -0300

    Aplica pipes de data e moeda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, ErrorHandler } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, ErrorHandler, LOCALE_ID } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule, PreloadAllModules } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy, registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt');
 
 import { MessageService } from 'primeng/components/common/messageservice';
 
@@ -41,7 +45,11 @@ import { ErrorHandlerService } from './core/services/error-handler.service';
       provide: HTTP_INTERCEPTORS,
       useClass: ErrorHandlerService,
       multi: true
-    }
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+    },
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
   bootstrap: [AppComponent]
--- src/app/despesas/despesas.module.ts
+++ src/app/despesas/despesas.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { DespesasPesquisaComponent } from './despesas-pesquisa/despesas-pesquisa.component';
+import { DespesaCadastroComponent } from './despesa-cadastro/despesa-cadastro.component';
 import { SharedModule } from '../shared/shared.module';
 import { DESPESA_ROUTES } from './despesas.routes';
 
 import { FieldsetModule } from 'primeng/fieldset';
 import { DropdownModule } from 'primeng/dropdown';
 import { CalendarModule } from 'primeng/calendar';
-import { SelectButtonModule } from 'primeng/selectbutton';
 import { TableModule } from 'primeng/table';
 
 
@@ -19,11 +19,13 @@ import { TableModule } from 'primeng/table';
     FieldsetModule,
     DropdownModule,
     CalendarModule,
-    SelectButtonModule,
     TableModule,
     SharedModule,
     RouterModule.forChild(DESPESA_ROUTES)
   ],
-  declarations: [DespesasPesquisaComponent]
+  declarations: [
+    DespesasPesquisaComponent,
+    DespesaCadastroComponent
+  ]
 })
 export class DespesasModule { }
