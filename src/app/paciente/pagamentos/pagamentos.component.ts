55026b1ca3a60f89e64ea1bc083774fbf81b6bd2
Leocaliban <leocaliban@gmail.com>
2019-05-27 03:05:56 -0300

    Cria os badges de pagamentos

--- src/app/paciente/pagamentos/pagamentos.component.ts
+++ src/app/paciente/pagamentos/pagamentos.component.ts
@@ -1,15 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-pagamentos',
   templateUrl: './pagamentos.component.html',
-  styleUrls: ['./pagamentos.component.scss']
+  styleUrls: ['./pagamentos.component.scss'],
+  animations: [
+    trigger('linha', [
+      state('pronto', style({
+        opacity: 1
+      })),
+      transition('void => pronto',
+        animate('300ms 0s ease-in',
+          keyframes([
+            style({
+              opacity: 0,
+              transform: 'translateX(-30px)',
+              offset: 0
+            }),
+            style({
+              opacity: 0.5,
+              transform: 'translateX(-20px)',
+              offset: 0.5
+            }),
+            style({
+              opacity: 0.8,
+              transform: 'translateX(-10px)',
+              offset: 0.8
+            }),
+            style({
+              opacity: 1,
+              transform: 'translateX(0px)',
+              offset: 1
+            })
+          ]))
+      ),
+      transition('pronto => void',
+        animate('300ms 0s ease-out',
+          keyframes([
+            style({
+              opacity: 1,
+              transform: 'translateX(0px)',
+              offset: 0
+            }),
+            style({
+              opacity: 0.8,
+              transform: 'translateX(-10px)',
+              offset: 0.2
+            }),
+            style({
+              opacity: 0,
+              transform: 'translateX(30px)',
+              offset: 1
+            })
+          ]))
+      )
+    ])
+  ]
 })
 export class PagamentosComponent implements OnInit {
+
+  activeTab = 'pronto';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  criarPagamento() {
+
   }
 
 }
