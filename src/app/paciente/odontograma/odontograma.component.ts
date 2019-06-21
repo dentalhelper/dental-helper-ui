190d3f4ecfd079a957132a3454fda7d58b39c25d
Leocaliban <leocaliban@gmail.com>
2019-06-21 04:43:08 -0300

    Implementa a exibição do odontograma

--- src/app/paciente/odontograma/odontograma.component.ts
+++ src/app/paciente/odontograma/odontograma.component.ts
@@ -1,15 +1,78 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-odontograma',
   templateUrl: './odontograma.component.html',
-  styleUrls: ['./odontograma.component.scss']
+  styleUrls: ['./odontograma.component.scss'],
+  animations: [
+    trigger('linha', [
+      state('pronto', style({
+        opacity: 1
+      })),
+      transition('void => pronto',
+        animate('300ms 0s ease-in',
+          keyframes([
+            style({
+              opacity: 0,
+              transform: 'translateX(-30px)',
+              offset: 0
+            }),
+            style({
+              opacity: 0.5,
+              transform: 'translateX(-20px)',
+              offset: 0.5
+            }),
+            style({
+              opacity: 0.8,
+              transform: 'translateX(-10px)',
+              offset: 0.8
+            }),
+            style({
+              opacity: 1,
+              transform: 'translateX(0px)',
+              offset: 1
+            })
+          ]))
+      ),
+      transition('pronto => void',
+        animate('300ms 0s ease-out',
+          keyframes([
+            style({
+              opacity: 1,
+              transform: 'translateX(0px)',
+              offset: 0
+            }),
+            style({
+              opacity: 0.8,
+              transform: 'translateX(-10px)',
+              offset: 0.2
+            }),
+            style({
+              opacity: 0,
+              transform: 'translateX(30px)',
+              offset: 1
+            })
+          ]))
+      )
+    ])
+  ]
 })
 export class OdontogramaComponent implements OnInit {
+
+  activeTab = 'pronto';
+
+  dentesTop = ['18', '17', '16', '15', '14', '13', '12', '11', '21', '22', '23', '24', '25', '26', '27', '28'];
+
+  dentesBot = ['48', '47', '46', '45', '44', '43', '42', '41', '31', '32', '33', '34', '35', '36', '37', '38'];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  alterarOdontograma() {
+
   }
 
 }
