edf4f6dd4e2cbb37c70f16fe2d25f94d1ca8ebef
Leocaliban <leocaliban@gmail.com>
2019-07-01 06:02:32 -0300

    Cria template de consultas do dia na dashboard

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  imagem: Object = {};
+
   constructor() { }
 
   ngOnInit() {
+    this.imagem = { 'background': `url(https://dok7xy59qfw9h.cloudfront.net/103/453/070/710003023-1q454qi-ikd82h9rpj1mna/original/1901_162643100560490_584881406_n.jpg) no-repeat`, 'background-size': 'cover' };
   }
 
 }
